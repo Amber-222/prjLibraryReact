ce1d812d64f44978f070891f2c0cd7bb18872bc1
Amber-222 <ST10287325@vcconnect.edu.za>
2025-09-04 09:32:34 +0200

    added helmet configuration

--- middleware/securityMiddleware.js
+++ middleware/securityMiddleware.js
@@ -2,3 +2,15 @@
 //csurf: how it helps with csrf attavks and configures for a node.js api
 //helmet: how it helps with header protections with click jacking
 //rate limiting, brute force prevention and libraries to include for this node.js api
+const helmet = require('helmet') //import helmet after running nmp i helmet in terminal
+const securityMiddleware = (app) => {
+    app.use(helmet({
+        contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+                'default-src': ["'self'"], //allows scripts from the website itself but nowhere else
+                'frame-ancestors': ["'none'"] //prevents this webiste from being embedded in another website to prevent clickjacking
+            }
+        }
+    }))
+}
