4b33ea7b142574a76cca17bc491318e5aa74702b
Amber-222 <ST10287325@vcconnect.edu.za>
2025-09-09 09:55:44 +0200

    added basic pages and routing
    METHODS MUST BE CAPITAL WHEN PASSED TO APP.JSX

---
+++ frontend/src/context/authContext.jsx
@@ -0,0 +1,21 @@
+//need required imports
+import { createContext, useContext, useState } from "react"
+
+//create the section of memory first for remembering if user is logged in
+const authContext = createContext()
+
+export function authProvider({children}) { //any child object this method has to interact with
+    const [isAuthenticated, setIsAuthenticated] = useState(false) //boolean variable tracking if user was authenticated and logged in
+
+    const login = () => setIsAuthenticated(true) //no parameters for this example
+    const logout = () => setIsAuthenticated(false) //no parameters for this example
+
+    return (
+        //providing ino from this context to the rest of the app to check status anywhere as needed with handling login and logout on the pages
+        <authContext.Provider value={{isAuthenticated, login, logout}}>
+
+        </authContext.Provider>
+    )
+}
+
+export const useAuth = () => useContext(authContext) //whenever useAuth is called it will use authContext file
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { authProvider } from './context/authContext.jsx' 
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <authProvider>
+      <App />
+    </authProvider>
   </StrictMode>,
 )
